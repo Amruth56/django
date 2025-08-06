761ac189bf68074f8f2d8c85b7ce6af4e00a3394
Amruth <amruth.mandappa56@gmail.com>
2025-08-06 15:48:20 +0530

    first dh=jango app part 3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,17 +1,30 @@
 from django.db import models
-
+import datetime 
+from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
+    
+    
+    def __str__(self):
+        return f"this is question {self.question_text} \n this is the id {self.id} \n this is the date {self.pub_date}"
   
+   
     
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+  
     choice_text = models.CharField(max_length = 200)
     votes  = models.IntegerField(default = 0)
+  
+    
+    def __str__(self):
+        return f"this is for the choice id: {self.id}\t question: {self.question}\t choice text: {self.choice_text}\t votes: {self.votes}"
     
 class Check(models.Model):
-    checkIOne  = models.ForeignKey(Choice, on_delete = models.CASCADE)
-    correct = models.BooleanField(default = False)
+    checkOne  = models.ForeignKey(Choice, on_delete = models.CASCADE)
+    correct123 = models.BooleanField(default = False)
+    
+    def __str__(self):
+        return f"this is for the Check table {self.checkOne}\t and {self.correct}"
 
     
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http  import HttpResponse 
+from .models import Question
+from django.http import Http404
+from django.template import loader
+
+
+
+def detail(request, question_i):
+    try:
+        question = Question.objects.get(pk = question_i)
+    except Question.DoesNotExist:
+        raise Http404("Quotatio does not exist")
+    question = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', question )
+        
+
+def results(request, question_id):
+    response = "You are looking at results of question %s."
+    return HttpResponse(response % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse(f"you are voting on question {question_id}.")
 
 
 def index(request):
-    return HttpResponse("Hello world 123")
+    latest_question_list = Question.objects.order_by('-pub_date')[:]
+    # context to pass data from view to template 
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
