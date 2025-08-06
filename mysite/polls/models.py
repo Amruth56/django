b6d908241383997ce42ef4ffd6f7a7270f4c2798
Amruth <amruth.mandappa56@gmail.com>
2025-08-06 10:33:59 +0530

    .

---
+++ mysite/polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+  
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes  = models.IntegerField(default = 0)
+    
+class Check(models.Model):
+    checkIOne  = models.ForeignKey(Choice, on_delete = models.CASCADE)
+    correct = models.BooleanField(default = False)
+
+    
